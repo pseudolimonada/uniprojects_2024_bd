0199363d91863245a0acda62f22bfd49842eaedd
Pedro Lima <pmc.lima@hotmail.com>
2024-05-02 19:41:04 +0100

    commit

--- main.py
+++ main.py
@@ -35,61 +35,86 @@ def landing_page():
     <br/>
     """
 
-@app.route('dbproj/register/<string:user_type>', methods=['POST'])
+@app.route('/dbproj/register/<string:user_type>', methods=['POST'])
 def register(user_type):
     # Registration logic here
+    if user_type == 'patient':
+        pass
+    elif user_type == 'assistant':
+        pass
+    elif user_type == 'nurse':
+        pass
+    elif user_type == 'doctor':
+        pass
+    else:
+        return """ERROR YOU STICH"""
     pass
 
-@app.route('dbproj/user', methods=['PUT'])
-def authenticate():
-    # Authentication logic here
-    pass
+# @app.route('/dbproj/user', methods=['PUT'])
+# def authenticate():
+#     # Authentication logic here
+#     pass
 
-@app.route('/appointment', methods=['POST'])
-def schedule_appointment():
+# @app.route('/dbproj/appointment', methods=['POST'])
+# def schedule_appointment():
     # Appointment scheduling logic here
-    pass
+#     pass
 
-@app.route('/appointments/<int:patient_user_id>', methods=['GET'])
-def see_appointments(patient_user_id):
-    # Fetch appointments logic here
-    pass
+# @app.route('/dbproj/appointments/<int:patient_user_id>', methods=['GET'])
+# def see_appointments(patient_user_id):
+#     # Fetch appointments logic here
+#     pass
 
-@app.route('/surgery', methods=['POST'])
-@app.route('/surgery/<int:hospitalization_id>', methods=['POST'])
-def schedule_surgery(hospitalization_id=None):
-    # Surgery scheduling logic here
-    pass
+# @app.route('/dbproj/surgery', methods=['POST'])
+# @app.route('/surgery/<int:hospitalization_id>', methods=['POST'])
+# def schedule_surgery(hospitalization_id=None):
+#     # Surgery scheduling logic here
+#     pass
 
-@app.route('/dbproj/prescriptions/<int:person_id>', methods=['GET'])
-def get_prescriptions(person_id):
-    # Fetch prescriptions logic here
-    pass
+# @app.route('/dbproj/prescriptions/<int:person_id>', methods=['GET'])
+# def get_prescriptions(person_id):
+#     # Fetch prescriptions logic here
+#     pass
 
-@app.route('/dbproj/prescription/', methods=['POST'])
-def add_prescriptions():
-    # Add prescriptions logic here
-    pass
+# @app.route('/dbproj/prescription/', methods=['POST'])
+# def add_prescriptions():
+#     # Add prescriptions logic here
+#     pass
 
-@app.route('/dbproj/bills/<int:bill_id>', methods=['POST'])
-def execute_payment(bill_id):
-    # Payment execution logic here
-    pass
+# @app.route('/dbproj/bills/<int:bill_id>', methods=['POST'])
+# def execute_payment(bill_id):
+#     # Payment execution logic here
+#     pass
 
-@app.route('/dbproj/top3', methods=['GET'])
-def list_top3_patients():
-    # Fetch top 3 patients logic here
-    pass
+# @app.route('/dbproj/top3', methods=['GET'])
+# def list_top3_patients():
+#     # Fetch top 3 patients logic here
+#     pass
 
-@app.route('/dbproj/daily/<string:date>', methods=['GET'])
-def daily_summary(date):
-    # Daily summary logic here
-    pass
+# @app.route('/dbproj/daily/<string:date>', methods=['GET'])
+# def daily_summary(date):
+#     # Daily summary logic here
+#     pass
 
-@app.route('/dbproj/report', methods=['GET'])
-def generate_monthly_report():
-    # Monthly report generation logic here
-    pass
+# @app.route('/dbproj/report', methods=['GET'])
+# def generate_monthly_report():
+#     # Monthly report generation logic here
+#     pass
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    logging.basicConfig(filename='log_file.log')
+    logger = logging.getLogger('logger')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    #create formatter
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s]:  %(message)s', '%H:%M:%S')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    host = '127.0.0.1'
+    port = 8080
+    app.run(host=host, debug=True, threaded=True, port=port)
+    logger.info(f'API v1.0 online: http://{host}:{port}')
