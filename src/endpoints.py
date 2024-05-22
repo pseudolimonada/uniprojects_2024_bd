f18f982470a3583c6a50e6e244bb6c4338734517
Pedro Lima <pmc.lima@hotmail.com>
2024-05-22 20:48:52 +0100

    updated register_user

--- src/endpoints.py
+++ src/endpoints.py
@@ -1,10 +1,11 @@
 
-from flask import Flask, g, request
+import flask
+import psycopg2
 import src.db as db
 import src.validator as validator
 from src.utils import config, StatusCodes, UserDetails, logger
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 #todo maybe implement a db connection pool
 
@@ -19,38 +20,46 @@ def landing_page():
 
 # Call this function when you want to use the db to get a db connection
 def get_db():
-    if 'db_con' not in g:
-        g.db_con = db.create_connection(config['DB_NAME'], config['DB_HOST'], config['DB_PORT'], config['DB_USER'], config['DB_PASS'])
-    return g.db_con
+    if 'db_con' not in flask.g:
+        flask.g.db_con = db.create_connection(config['DB_NAME'], config['DB_HOST'], config['DB_PORT'], config['DB_USER'], config['DB_PASS'])
+    return flask.g.db_con
 
 # This function gets called when the request is done to close db connection
 @app.teardown_appcontext
 def close_db(e=None):
-    db_con = g.pop('db', None)
+    db_con = flask.g.pop('db_con', None)
 
     if db_con is not None:
         db_con.close()
 
 @app.route('/dbproj/register/<string:user_type>', methods=['POST'])
 def register(user_type):
-    logger.info('POST /register')
-    payload = request.get_json()
+    payload = flask.request.get_json()
 
+    logger.info('POST /register')
     logger.debug(f'POST /register - payload received: {payload}')
-    
+
+    # check if user type is valid
     if (validator.user_type(user_type) == False):
-        response = {'status': StatusCodes['api_error'], 'results': 'Invalid user type'}
-        return response
+        response = {'status': StatusCodes['api_error'], 'errors': 'Invalid user type'}
+        return flask.jsonify(response)
     
+    # check if all required fields are present
     missing_args = validator.user_register_details(user_type, payload)
     if len(missing_args) != 0:
-        response = {'status': StatusCodes['api_error'], 'results': f'Missing arguments: {', '.join(missing_args)}'}
-        return response
+        response = {'status': StatusCodes['api_error'], 'errors': f'Missing arguments: {', '.join(missing_args)}'}
+        return flask.jsonify(response)
     
     # add to register
-    db.register_user(get_db(), user_type, payload)
+    try:
+        db.register_user(get_db(), user_type, payload)
+        response = {'status': StatusCodes['success'], 'results': f'Good job fam'}
+    except [Exception,psycopg2.DatabaseError] as e:
+        logger.error(f"Error: {e}")
+        response = {'status': StatusCodes['internal_error'], 'errors': str(e)}
     
-    return {'status': StatusCodes['success'], 'results': f'Good job fam'}
+    logger.debug(f'POST /register - response: {response}')
+    return flask.jsonify(response)
 
 
 # @app.route('/dbproj/user', methods=['PUT'])
@@ -89,10 +98,17 @@ def register(user_type):
 #     # Payment execution logic here
 #     pass
 
-# @app.route('/dbproj/top3', methods=['GET'])
-# def list_top3_patients():
-#     # Fetch top 3 patients logic here
-#     pass
+@app.route('/dbproj/top3', methods=['GET'])
+def list_top3_patients():
+    logger.info('GET /top3')
+    try:
+        db.get_top3_patients(get_db())
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return {'status': StatusCodes['internal_error'], 'errors': str(e)}
+
+    return {'status': StatusCodes['success'], 'results': 'Top 3 patients listed'}
+    pass
 
 # @app.route('/dbproj/daily/<string:date>', methods=['GET'])
 # def daily_summary(date):
