571faced217c16b65c8fc0fb399078edac457e0e
Pedro Lima <pmc.lima@hotmail.com>
2024-06-06 11:19:10 +0100

    small logger fix

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from src.utils import logger, config
 
 if __name__ == '__main__':
     app.run(host=config['APP_HOST'], debug=True, threaded=True, port=config['APP_PORT'])
-    logger.info(f'API v1.0 online: http://{config['APP_HOST']}:{config['APP_PORT']}')
+    logger.info(f'API v1.0 online: http://{config["APP_HOST"]}:{config["APP_PORT"]}')
